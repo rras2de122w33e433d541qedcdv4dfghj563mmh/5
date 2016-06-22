7e186a25128ab65dc11726b3fe6674a93869fda4
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-06-22 11:58:18 -0700

    Create feedback.lua

---
+++ plugins/feedback.lua
@@ -0,0 +1,28 @@
+do
+
+ function run(msg, matches)
+ local ch = 'channel#id'..msg.to.id
+ local fuse = '📌 #فیدبک جدید\n\n👤 نام کاربر : ' .. msg.from.print_name .. '\n\n👤 نام کاربری : @' .. msg.from.username ..'\n\n👤 کد کاربر : ' .. msg.from.id ..'\n\n👤 کد گروه : '..msg.to.id.. '\n\n📝 متن پیام :\n\n' .. matches[1]
+ local fuses = '!printf user#id' .. msg.from.id
+
+
+   local text = matches[1]
+   local chat = "channel#id"..1063728845
+
+  local sends = send_msg(chat, fuse, ok_cb, false)
+  return '✅ Message successfully sent'
+
+ end
+ end
+ return {
+
+  description = "Feedback",
+
+  usage = "feedback: Send A Message To Admins.",
+  patterns = {
+  "^[!/][Ff]eedback (.*)$",
+  "^[Ff]eedback (.*)$"
+  
+  },
+  run = run
+ }
