371657c0aa35abcfa0e5b23dfd6c282e156f5641
rras2de122w33e433d541qedcdv4dfghj563mmh <sdkey78@gmail.com>
2016-06-28 10:50:16 -0700

    Update stats.lua

--- plugins/stats.lua
+++ plugins/stats.lua
@@ -144,7 +144,7 @@ return {
     "^[#!/]([Ss]tatslist)$",
     "^[#!/]([Ss]tats) (group) (%d+)",
     "^[#!/]([Ss]tats) (turbo)",
-	"^[#!/]([Tt]urbo)"
+--	"^[#!/]([Tt]urbo)"
     }, 
   run = run
 }
